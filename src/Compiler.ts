9c289ded4f9158e5bfd224e3ef7f462ac600160e
hein.66 <hein.66@osu.edu>
2023-03-31 15:55:46 -0400

    Converted project into package based repo

--- src/Compiler.ts
+++
@@ -1,9 +0,0 @@
-import * as ts from "typescript";
-
-export function compile(
-  fileNames: string[],
-  options: ts.CompilerOptions
-): void {
-  let program = ts.createProgram(fileNames, options);
-  let emitResult = program.emit();
-}

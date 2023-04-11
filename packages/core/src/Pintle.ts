dcb67b641d506c893956c3d324c1d15bf6d92c6b
hein.66 <hein.66@osu.edu>
2023-04-11 17:43:55 -0400

    Seperated out functionality to private member

--- packages/core/src/Pintle.ts
+++ packages/core/src/Pintle.ts
@@ -3,34 +3,66 @@ import * as fs from "fs";
 import {defaultPintleOptions, parseOptions, PintleOptions} from "./PintleOptions";
 import {OutputFileTypes} from "./FileOptions";
 
+interface ResourceGroups {
+  name: string;
+  resources: object[];
+  filename: string;
+}
 
 export class Pintle {
 
-  constructor(private options: PintleOptions = defaultPintleOptions) {
+  private options: PintleOptions;
+
+  private resourceGroups: ResourceGroups[] = [];
+
+  constructor(options: PintleOptions = defaultPintleOptions) {
     //Parse options and log
     this.options = parseOptions(options);
     console.log("Options:", options);
   }
 
   public add(name: string, resources: object[]) {
-    const fileEnding = this.options.file?.type
-    const filename = name + "." + fileEnding;
-    const location = this.options.file?.outputDir + "/" + filename;
+    const filename = this.determineFilename(name);
+    this.resourceGroups.push({
+      name,
+      resources,
+      filename
+    });
 
     //Create definition as JSON or YAML
-    const definition = (this.options.file?.type === OutputFileTypes.YAML) ?
-      YAML.stringify(resources, {
+    // const definition = (this.options.file?.type === OutputFileTypes.YAML) ?
+    //   YAML.stringify(resources, {
+    //
+    //   }) : JSON.stringify(resources, null, 3);
 
-      }) : JSON.stringify(resources, null, 3);
 
 
-
-    fs.appendFileSync(location, definition);
+    //fs.appendFileSync(location, definition);
 
 
     // resources.forEach(definition => {
     //   this.buildDefinition(name, definition);
     // });
+  }
+
+  public addMany(resourceArray: {[name: string]: object[]}) {
+    const resourceEntries = Object.entries(resourceArray);
+    resourceEntries.forEach(entry => {
+      const name = entry[0];
+      const resources = entry[1];
+      this.add(name, resources)
+    });
+  }
+
+  public buildYaml() {
+    this.resourceGroups.forEach(resourceGroup => {
+      const filename = resourceGroup.filename;
+      fs.appendFileSync(resourceGroup.filename, "---\r\n");
+    });
+  }
+
+  public buildJson() {
+
   }
 
   private buildDefinition(name: string, resource: object) {
@@ -53,13 +85,10 @@ export class Pintle {
     }
   }
 
-  // public addAll(resourceGroups: {[key: string]: ResourceGroup }) {
-  //   //
-  //   const resourceGroupEntries = resourceGroups ? Object.entries(resourceGroups) : [];
-  //   resourceGroupEntries.forEach(resourceGroup => {
-  //
-  //
-  //   });
-  // }
+  determineFilename(name: string) {
+    const fileEnding = this.options.file?.type;
+    const fileWithEnding = name + "." + fileEnding;
+    return this.options.file?.outputDir + "/" + fileWithEnding;
+  }
 }
 

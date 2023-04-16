47f07d04aa3646b9655e543f7b1c4f8a04488611
Noah Hein <noahhein123@gmail.com>
2023-04-15 23:36:17 -0400

    Set up very basic cli test environment

--- packages/pintle-cli/src/index.ts
+++ packages/pintle-cli/src/index.ts
@@ -1,4 +1,15 @@
-function test() {
-  console.log("FUCKKKKK!")
-}
-test()
+import * as yargs from "yargs";
+
+yargs
+  .scriptName("pintle-cli")
+  .command("hello", "does some shit", (yargs) => {
+    yargs.positional('name', {
+      type: 'string',
+      default: 'Cambi',
+      describe: 'the name to say hello to'
+    })
+  }, (argv) => {
+    console.log("What the fuck is going on")
+  })
+  .help()
+  .argv

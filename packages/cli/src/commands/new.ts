dc84a8a17f0c2b9b9d72b934fd2d65f8671cc2d1
Noah Hein <noahhein123@gmail.com>
2023-04-23 15:45:53 -0400

    Trying to use inquirer

--- packages/cli/src/commands/new.ts
+++ packages/cli/src/commands/new.ts
@@ -1,22 +1,13 @@
 import { Command } from "./command";
-import * as prompts from "prompts";
+
 export class NewCommand extends Command {
 
 
   async run() {
+    const inquirer = await import("inquirer");
+    const prompt = inquirer.createPromptModule();
 
 
-    const response = await prompts({
-      type: 'multiselect',
-      name: 'color',
-      message: 'Pick colors',
-      choices: [
-        { title: 'Red', value: '#ff0000' },
-        { title: 'Green', value: '#00ff00' },
-        { title: 'Blue', value: '#0000ff' }
-      ]
-    });
 
-    console.log(response)
   }
 }

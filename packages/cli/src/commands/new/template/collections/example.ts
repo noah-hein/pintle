034d799d876e036e83a4f142287562f480361f4f
hein.66 <hein.66@osu.edu>
2023-04-28 12:08:24 -0400

    Removed example and old config

--- packages/cli/src/commands/new/template/collections/example.ts
+++
@@ -1,8 +0,0 @@
-import { ResourceFile, Resources } from "@pintle/core";
-import { Templates } from "@pintle/templates";
-
-export class Example extends ResourceFile {
-  resources(): Resources {
-    return [Templates.namespace("foobar")];
-  }
-}

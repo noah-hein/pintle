f4ff78b160f4a75c5261a4210f6fafe698bb910c
hein.66 <hein.66@osu.edu>
2023-05-02 15:22:01 -0400

    Delete build command

--- packages/cli/src/commands/build/build.yargs.ts
+++
@@ -1,8 +0,0 @@
-import { CommandModule } from "yargs";
-import { BuildCommand } from "./build.command";
-
-export const buildYargsCommand: CommandModule = {
-  command: "build",
-  describe: "Finds and compiles all resources into a configurable distribution",
-  handler: async () => new BuildCommand().run()
-}

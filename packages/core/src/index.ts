47e58e20600a0f7c45d8ab063051fa7dd7f1109d
hein.66 <hein.66@osu.edu>
2023-05-08 11:22:15 -0400

    Fixed util export

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,6 +2,5 @@ export * from "./pintle";
 export * from "./pintle.config";
 export * from "./resource";
 export * from "./folder";
-export {isModule} from "./module";
-export {Modules} from "./module";
-export {Module} from "./module";
+export * from "./module";
+export * from "./util"

2f086e2003bd79eccb7332e2eaa17f2ebfedd676
hein.66 <hein.66@osu.edu>
2023-05-08 11:33:34 -0400

    Trying to fix run command

--- packages/cli/src/commands/run/run.command.ts
+++ packages/cli/src/commands/run/run.command.ts
@@ -1,6 +1,8 @@
 import * as shell from "shelljs";
 import { Command } from "../command";
 import { RunCommandOptions } from "./run.yargs";
+import * as path from "path";
+import * as process from "process";
 
 export class RunCommand extends Command {
   private runOptions: RunCommandOptions;
@@ -11,7 +13,7 @@ export class RunCommand extends Command {
   }
 
   async run() {
-    const mainPath = this.runOptions.main;
+    const mainPath = path.join(process.cwd(), "src/main");
     shell.exec("ts-node " + mainPath);
   }
 }

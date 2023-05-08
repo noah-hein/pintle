640ea3d4834d5c0a973ffcc02a56240de733b1ac
hein.66 <hein.66@osu.edu>
2023-05-08 10:42:31 -0400

    Added clean command

--- packages/cli/src/commands/clean/clean.command.ts
+++ packages/cli/src/commands/clean/clean.command.ts
@@ -1,7 +1,12 @@
 import { Command } from "../command";
+import {rimraf} from "rimraf";
+import * as process from "process";
+import * as path from "path";
 
 export class CleanCommand extends Command {
   run() {
-    console.log("Add some stuff");
+    const outputPath = path.join(process.cwd(), "/generated/*");
+    console.log("Deleting files in" + outputPath)
+    rimraf.sync(outputPath);
   }
 }

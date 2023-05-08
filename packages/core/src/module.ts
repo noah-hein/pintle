3d95e922c167dfc0a2268376bdd089c00e1eb066
hein.66 <hein.66@osu.edu>
2023-05-08 12:31:20 -0400

    Converted module into a class

--- packages/core/src/module.ts
+++ packages/core/src/module.ts
@@ -1,17 +1,16 @@
 import {Resources} from "./resource";
 
-export interface Module {
-  name: string;
-  resources?: Resources;
-  modules?: Modules;
-}
-
-// export class Module {
-//   name: string = "default";
+// export interface Module {
+//   name: string;
 //   resources?: Resources;
 //   modules?: Modules;
 // }
 
+export class Module {
+  name: string = "";
+  resources?: Resources;
+  modules?: Modules;
+}
 export type Modules = Module[];
 
 export function isModule(object: unknown): object is Module {

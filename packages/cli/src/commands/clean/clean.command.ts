c42ae22b4a8e8e5ca1bdbcb1500cfb3b64ecb5c3
Noah Hein <noahhein123@gmail.com>
2023-05-08 20:02:21 -0400

    Clean command uses config

--- packages/cli/src/commands/clean/clean.command.ts
+++ packages/cli/src/commands/clean/clean.command.ts
@@ -2,10 +2,12 @@ import { Command } from "../command";
 import {rimraf} from "rimraf";
 import * as process from "process";
 import * as path from "path";
+import { findConfig } from "@pintle/core";
 
 export class CleanCommand extends Command {
-  run() {
-    const outputPath = path.join(process.cwd(), "/generated");
+  async run() {
+    const config = await findConfig();
+    const outputPath = path.join(process.cwd(), config.outputPath);
     console.log("Deleting files in" + outputPath)
     rimraf.sync(outputPath);
   }
